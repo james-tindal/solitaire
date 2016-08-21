927dc53e34673eb2c40a8d4c98a209ef10527098
James Brown <github@mesbrown.co.uk>
2016-08-21 14:00:29 +0100

    Undoes one step at a time. Wont undo initial table

--- src/undo.js
+++ src/undo.js
@@ -1,4 +1,4 @@
-import { compose, either, map, when, head, prop } from 'ramda'
+import { compose, either, map, when, last, prop } from 'ramda'
 import filter from 'flyd/module/filter'
 import sampleOn from 'flyd/module/sampleon'
 import Action from 'actions'
@@ -6,17 +6,19 @@ import keyboard from 'keyboardjs'
 
 
 const is = type => action => action.value === Action[type]().value
-
+const dealOrReset = either( is('Deal'), is('Reset') )
+const drawOrMove = either( is('Draw'), is('Move') )
 
 let tableHistory = []
 let push = true
 
 export default
 ( action$, model$ ) => {
-  const drawMove$ = filter( either( is('Draw'), is('Move') ))( action$ )
+  const drawMove$ = filter( either( dealOrReset, drawOrMove ))( action$ )
   const table$ = compose( map( prop( 'table' )), sampleOn( drawMove$ ))( model$ )
-  action$.map( when( either( is('Deal'), is('Reset') ), _ => tableHistory = [] ))
-  table$.map( table => push ? tableHistory.push(table) : push = true )
+  action$.map( when( dealOrReset, _ => tableHistory = [] ))
+  table$.map( [].push.bind( tableHistory ))
+  model$.map( _ => console.log(tableHistory.length, tableHistory))
   keyboard.bind( 'command + z', _ =>
-    action$( Action.Undo({ table: ( push = false, tableHistory.pop(), tableHistory.pop() ) })))
+    tableHistory.length > 1 && action$( Action.Undo({ table: tableHistory.pop() })))
 }
