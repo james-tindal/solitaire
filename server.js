91282d9ca1908fc81af66a4a19f5023658e7a608
james-tindal <10291002+james-tindal@users.noreply.github.com>
2024-07-16 14:26:17 +0100

    various

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config');
-const PORT = process.argv[2]
+var path = require('path')
+var express = require('express')
+const PORT = process.argv[2] || 8000
 
-var app = express();
+var app = express()
 
 app.use(express.static(__dirname + '/public'))
 
 app.listen(PORT, 'localhost', function(err) {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
 
-  console.log(`Listening at http://localhost:${PORT}`);
-});
+  console.log(`Listening at http://localhost:${PORT}`)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const webpack = require("webpack")
-const { resolve } = require("path")
-const { argv: flags } = require("yargs")
+const webpack = require('webpack')
+const { resolve } = require('path')
+const { argv: flags } = require('yargs')
 
-const production = "p" in flags
+const production = 'p' in flags
 
 module.exports = {
-  entry: "./src",
-  devtool: production ? false : "eval-source-map",
-  output: { path: resolve("public"), filename: "build.js", publicPath: "" },
-  resolve: { root: resolve("src"), extensions: ["", ".js"] },
+  entry: './src',
+  devtool: production ? false : 'eval-source-map',
+  output: { path: resolve('public'), filename: 'build.js', publicPath: '' },
+  resolve: { root: resolve('src'), extensions: ['', '.js'] },
   plugins: production
     ? [
-        new webpack.DefinePlugin({ "process.env.NODE_ENV": '"production"' }),
+        new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'production'" }),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false }})
       ]
     : [new webpack.DefinePlugin({ DEBUG: true }), new webpack.NoErrorsPlugin()],
   module: {
-    loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel" }]
+    loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel' }]
   }
 }
